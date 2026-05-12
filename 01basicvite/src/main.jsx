45c642ff6508bd75356bff3f7bac51ed078836d8
bhavya sachula <bhavyasachula07@gmail.com>
2026-05-12 22:25:57 +0530

    Getting started to react

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+function DisplaySM(){
+      return(
+            <a href='https://www.google.com' target='_blank'>Visit googlee</a>
+      )
+}
+const reactElement = (<h1>baapo ka baap bhavya</h1>)
 createRoot(document.getElementById('root')).render(
+      <div>
+      <DisplaySM/>
+      <br/>
+      reactElement
+      </div>
       
-      <App />
-  
 )
