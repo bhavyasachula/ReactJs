b8d0a904d25528b54abf7d22341f403ba3f955c8
bhavya sachula <bhavyasachula07@gmail.com>
2026-05-15 14:58:54 +0530

    added some more colors for backgroundChanger

--- TailwindCssAndProps/src/App.jsx
+++ TailwindCssAndProps/src/App.jsx
@@ -7,21 +7,29 @@ function App(Props) {
 
   return (
     <>
-    <div className='flex flex-warp gap-10 h-screen justify-center items-center ' style={{backgroundColor:color}}>
-
-      <button className='bg-red-700 p-2 rounded-2xl'
+    <div className='w-full h-screen duration-100 ' style={{backgroundColor:color}}>
+    <div className='border-t-2 border-b-4 fixed rounded-2xl flex gap-10 flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+      <button className='text-white bg-red-700 p-2 rounded-2xl'
       onClick={()=>{
         setColor("red")
       }}>Red</button>
-        <button className='bg-blue-700 p-2 rounded-2xl'
+        <button className='text-white bg-blue-700 p-2 rounded-2xl'
       onClick={()=>{
         setColor("blue")
       }}>blue</button>
-        <button className='bg-green-700 p-2 rounded-2xl'
+        <button className='text-white bg-green-700 p-2 rounded-2xl'
       onClick={()=>{
         setColor("green")
       }}>green</button>
-      
+      <button className='text-white bg-black p-2 rounded-2xl'
+      onClick={()=>{
+        setColor("black")
+      }}>black</button>
+      <button className=' text-white bg-purple-700 p-2 rounded-2xl'
+      onClick={()=>{
+        setColor("purple")
+      }}>purple</button>
+      </div>
     </div>
     </>
   )
