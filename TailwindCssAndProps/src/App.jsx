4b86f03106e8606a7aac799a2eb693bc13908f50
bhavya sachula <bhavyasachula07@gmail.com>
2026-05-15 15:01:42 +0530

    working on border backgroundChanger

--- TailwindCssAndProps/src/App.jsx
+++ TailwindCssAndProps/src/App.jsx
@@ -8,24 +8,25 @@ function App(Props) {
   return (
     <>
     <div className='w-full h-screen duration-100 ' style={{backgroundColor:color}}>
-    <div className='border-t-2 border-b-4 fixed rounded-2xl flex gap-10 flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-      <button className='text-white bg-red-700 p-2 rounded-2xl'
+    <div className='fixed rounded-2xl flex gap-10 flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+
+      <button className='text-white border-b-4 bg-red-700 p-2 rounded-xl'
       onClick={()=>{
         setColor("red")
       }}>Red</button>
-        <button className='text-white bg-blue-700 p-2 rounded-2xl'
+        <button className='text-white border-b-4 bg-blue-700 p-2 rounded-xl'
       onClick={()=>{
         setColor("blue")
       }}>blue</button>
-        <button className='text-white bg-green-700 p-2 rounded-2xl'
+        <button className='text-white border-b-4 bg-green-700 p-2 rounded-xl'
       onClick={()=>{
         setColor("green")
       }}>green</button>
-      <button className='text-white bg-black p-2 rounded-2xl'
+      <button className='text-white border-b-4 bg-black p-2 rounded-xl'
       onClick={()=>{
         setColor("black")
       }}>black</button>
-      <button className=' text-white bg-purple-700 p-2 rounded-2xl'
+      <button className=' text-white border-b-4 bg-purple-700 p-2 rounded-xl'
       onClick={()=>{
         setColor("purple")
       }}>purple</button>
