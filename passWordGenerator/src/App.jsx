154d2b5cfd57860a4b2cb6ecf2727c0bfcf6216d
bhavya sachula <bhavyasachula07@gmail.com>
2026-05-16 15:34:05 +0530

    Password generator using react

---
+++ passWordGenerator/src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect,useState,useCallback } from 'react'
+
+function App() {
+  const [length, setlength] = useState(8)
+  const [numberAllowed,setNumberAllowed] = useState(false)
+  const [charAllowed,setCharAllowed] = useState(false)
+  const [password,setPassword] = useState("")
+
+  const passwordGenerator =  useCallback(()=>{
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str +="0123456789"
+    if(charAllowed) str += "~!@$%^&*(){}+=[]"
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
+    }
+    
+    setPassword(pass)
+
+  },[length,numberAllowed,charAllowed])
+    
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator])
+
+  return (
+    <>
+      <div className='w-full  max-w-md mx-auto shadow-md rounded-lg p-4 my-8 bg-red-100 '> <div className='flex shadow rounded-lg overflow-hidden mb-4 '>
+        <input type="text" 
+          value={password}
+          className='outline-none  w-full py-1 px-3'/>
+          <button className='p-2 outline-none bg-blue-600 text-white'>copy</button>
+      </div></div>  
+     
+    </>
+  )
+}
+
+export default App
