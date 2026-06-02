3986388410adcccee5e73ac94d27e24dd94581f4
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-02 23:57:29 +0530

    infinite button color change

--- basicsForReact/src/components/Button.jsx
+++ basicsForReact/src/components/Button.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
 
-function Button({text}) {
-  const [color,colorChanger] = useState(["red","blue","green","yellow"])
-const handleonclick=()=>{
-  colorChanger(["blue","green","yellow"])
-}
+function Button({ text }) {
+  const colors = ["red", "blue", "green", "yellow"]
+
+  const [index, setIndex] = useState(0)
+
+  const handleOnClick = () => {
+    setIndex((index + 1) % colors.length)
+  }
+
   return (
-    <>
-    <button onClick={handleonclick}
-    className={`bg-${color}-600`}>{text}</button>
-    </>
-    )
+    <button
+      onClick={handleOnClick}
+      className={
+        index === 0 ? "bg-red-600" :
+        index === 1 ? "bg-blue-600" :
+        index === 2 ? "bg-green-600" :
+        "bg-yellow-600"
+      }
+    >
+      {text}
+    </button>
+  )
 }
 
 export default Button
