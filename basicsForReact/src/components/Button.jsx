4e2b9909c6d56c85a0e6520ad96f6441e19cef6e
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-02 23:58:17 +0530

    trying some diffrent technique to chaneg the color

--- basicsForReact/src/components/Button.jsx
+++ basicsForReact/src/components/Button.jsx
@@ -1,27 +1,16 @@
 import React, { useState } from 'react'
 
-function Button({ text }) {
-  const colors = ["red", "blue", "green", "yellow"]
-
-  const [index, setIndex] = useState(0)
-
-  const handleOnClick = () => {
-    setIndex((index + 1) % colors.length)
-  }
-
+function Button({text}) {
+  const [color,colorChanger] = useState(["red","blue","green","yellow"])
+const handleonclick=()=>{
+  colorChanger(["blue","green","yellow"])
+}
   return (
-    <button
-      onClick={handleOnClick}
-      className={
-        index === 0 ? "bg-red-600" :
-        index === 1 ? "bg-blue-600" :
-        index === 2 ? "bg-green-600" :
-        "bg-yellow-600"
-      }
-    >
-      {text}
-    </button>
-  )
+    <>
+    <button onClick={handleonclick}
+    className={`bg-${color}-600`}>{text}</button>
+    </>
+    )
 }
 
 export default Button
