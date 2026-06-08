610a716e59379f243827be1d54fd3142757110fd
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-08 23:55:19 +0530

    more css justifying

--- carausol/src/App.jsx
+++ carausol/src/App.jsx
@@ -7,14 +7,14 @@ function App() {
 
   return (
       <>
-      <div className='flex flex-column justify container border-6 border-indigo-500 h-full w-screenp-2'>
-       <div className='group h-100 w-10 text-center'>
+      <div className='flex flex-column justify-center container  h-full w-screen'>
+       <div className='group h-1 w-10 text-center'>
           <div className='bg-red-100 w-10'>1<img src="" alt="" /></div>
           <div className='bg-blue-100 w-10'>2<img src="" alt="" /></div>
           <div className='bg-yellow-100 w-10' >3<img src="" alt="" /></div>
           <div className='bg-green-100 w-10'>4<img src="" alt="" /></div>
         </div>
-        </div>
+      </div>
       </>
   )
 }
