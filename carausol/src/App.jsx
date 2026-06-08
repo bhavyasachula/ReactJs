c8a562e5f1276f33c512acc23fc8b0c8194e8a08
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-08 23:54:17 +0530

    more css shit

--- carausol/src/App.jsx
+++ carausol/src/App.jsx
@@ -7,12 +7,12 @@ function App() {
 
   return (
       <>
-      <div className='flex justify container border-6 border-indigo-500 h-full w-screenp-2'>
-       <div className='group h-100 w-100 text-center'>
-          <div className='bg-red-100'>1<img src="" alt="" /></div>
-          <div className='bg-blue-100'>2<img src="" alt="" /></div>
-          <div className='bg-yellow-100' >3<img src="" alt="" /></div>
-          <div className='bg-green-100'>4<img src="" alt="" /></div>
+      <div className='flex flex-column justify container border-6 border-indigo-500 h-full w-screenp-2'>
+       <div className='group h-100 w-10 text-center'>
+          <div className='bg-red-100 w-10'>1<img src="" alt="" /></div>
+          <div className='bg-blue-100 w-10'>2<img src="" alt="" /></div>
+          <div className='bg-yellow-100 w-10' >3<img src="" alt="" /></div>
+          <div className='bg-green-100 w-10'>4<img src="" alt="" /></div>
         </div>
         </div>
       </>
