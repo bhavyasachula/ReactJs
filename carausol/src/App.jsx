88d43096d883fe22e48a4ae1616a9d7c0ca3bbfb
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-08 23:47:43 +0530

    handling the border

--- carausol/src/App.jsx
+++ carausol/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
       <>
       <div class></div>
-       <div className='container border-[black]-200 h-100 w-100 text-center'>
+       <div className='container border-4 h-100 w-100 text-center'>
           <div className='bg-red-100'>1<img src="" alt="" /></div>
           <div className='bg-blue-100'>2<img src="" alt="" /></div>
           <div className='bg-yellow-100' >3<img src="" alt="" /></div>
