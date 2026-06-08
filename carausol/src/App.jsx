2c6deaa96c98702fba2a634e3d44f53fb596759c
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-08 23:46:00 +0530

    Cleaning up the folder

--- carausol/src/App.jsx
+++ carausol/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <>
-        <div className='Container b-1 h-100 w-100 '>
+      <div class></div>
+       <div className='container border-[black]-200 h-100 w-100 text-center'>
           <div className='bg-red-100'>1<img src="" alt="" /></div>
           <div className='bg-blue-100'>2<img src="" alt="" /></div>
           <div className='bg-yellow-100' >3<img src="" alt="" /></div>
---
+++ carausol/src/boxes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function boxes() {
+  return (
+    <div>boxes</div>
+  )
+}
+
+export default boxes
