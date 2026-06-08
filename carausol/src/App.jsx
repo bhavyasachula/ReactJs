7007046976be15c3f0d8393eaf5231d24887f937
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-08 23:53:00 +0530

    handling the css

--- carausol/src/App.jsx
+++ carausol/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
       <>
-      <div className='container border-6 border-indigo-500 p-2'>
-       <div className='group border-b-2  h-100 w-100 text-center'>
+      <div className='flex justify container border-6 border-indigo-500 h-full w-screenp-2'>
+       <div className='group h-100 w-100 text-center'>
           <div className='bg-red-100'>1<img src="" alt="" /></div>
           <div className='bg-blue-100'>2<img src="" alt="" /></div>
           <div className='bg-yellow-100' >3<img src="" alt="" /></div>
