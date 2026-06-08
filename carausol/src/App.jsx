3456665e06a865fd51df527713424623213ffc61
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-08 23:50:48 +0530

    handling the divs

--- carausol/src/App.jsx
+++ carausol/src/App.jsx
@@ -7,12 +7,13 @@ function App() {
 
   return (
       <>
-      <div class></div>
-       <div className='container border-4 h-100 w-100 text-center'>
+      <div className='container border-6 border-indigo-500 p-2'>
+       <div className='group border-b-2  h-100 w-100 text-center'>
           <div className='bg-red-100'>1<img src="" alt="" /></div>
           <div className='bg-blue-100'>2<img src="" alt="" /></div>
           <div className='bg-yellow-100' >3<img src="" alt="" /></div>
           <div className='bg-green-100'>4<img src="" alt="" /></div>
+        </div>
         </div>
       </>
   )
