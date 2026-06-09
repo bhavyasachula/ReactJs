82fd8b8b5a32deecbeef92a6e9c021e424e42177
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-09 14:04:29 +0530

    Creating fresh caraousal

---
+++ Caraousal/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='Container border-2-black'></div>
+    </>
+  )
+}
+
+export default App
--- carausol/src/boxes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function boxes() {
-  return (
-    <div>boxes</div>
-  )
-}
-
-export default boxes
