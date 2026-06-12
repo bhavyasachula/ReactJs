b0db847c6bf80e8e163bc27feb75cc1645acdd70
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-12 19:46:29 +0530

    feat: implementing the api route for database

--- Caraousal/server/server.js
+++ Caraousal/server/server.js
@@ -3,28 +3,34 @@ const cors = require("cors")
 const app = express()
 const PostgresConn = require("./databasePg")
 
-PostgresConn.connect().then(()=>{
-    console.log("Connected");
-})
+
 app.use(cors())
 
 async function connection() {
-  const result = await PostgresConn.query(
-    'SELECT * FROM "sliderUrl"'
-  );
-
-  return result.rows;
+PostgresConn.connect().then(()=>{
+    console.log("database Connected");
+})
 }
 
-console.log(connection().then((data)=>{
-    console.log(data);
-    
-}));
+connection()
+
+// connection().then((data)=>{
+//     console.log(data); 
+// });
 
 app.get("/",(req,res)=>{
     return res.send("server is running")
-})
+ })
 
+//   const result = await PostgresConn.query(
+//     'SELECT * FROM "sliderUrl"'
+//   );
+
+//   return result.rows;
+
+app.get("/upload",(req,res)=>{
+    console.log(req.body);
+})
 app.listen("2000",()=>{
     console.log("http://localhost:2000/")
 })
