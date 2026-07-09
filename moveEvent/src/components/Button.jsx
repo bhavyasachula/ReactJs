2a5b8611c4bde1312631c28ed21e2f8d565c8c1a
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-09 23:12:06 +0530

    Added a transition to make it look more smoother

--- moveEvent/src/components/Button.jsx
+++ moveEvent/src/components/Button.jsx
@@ -1,31 +1,57 @@
 import React, { useState } from 'react'
 
 function Button() {
-    const [position,setPosition] = useState({x:0,y:0});
-    const handleMouseMove=(e)=>{
-        const rect = e.currentTarget.getBoundingClientRect();
-        const x = e.ClientX - rect.left;
-        const y = e.ClientY - rect.top;
-        setPosition(x,y);
-    }
-  
+  const boxSize = 70
+  const arrowSize = 25
+  const half = arrowSize / 2
+
+  const [position, setPosition] = useState({
+    x: boxSize / 2,
+    y: boxSize / 2,
+  })
+  const handleMouseLeave=(e)=>{
+    
+    setPosition({x:35,y:35})
+    
+  }
+
+  const handleMouseMove = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect()
+
+    let x = e.clientX - rect.left
+    let y = e.clientY - rect.top
+
+    
+    x = Math.max(half, Math.min(x, rect.width - half))
+    y = Math.max(half, Math.min(y, rect.height - half))
+
+    setPosition({ x, y })
+  }
+
   return (
-    <>
-    <div className='Box border h-screen w-screen flex justify-center items-center '>
-        <div className='innerBox rounded-3xl h-[70px] w-[70px] flex justify-center items-center p-2'
-        onMouseMove={handleMouseMove}>
-            <div className='follow  p-1'
-            style={{
-                left:`${position.x}px`,
-                top:`${position.y}px`,
-                transform:'translate(-50%,-50%)'
-            }}
-            >
-                ↑
-            </div>
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <div
+        className='innerBox relative h-[70px] w-[70px] rounded-3xl border overflow-hidden m-2'
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div
+          className='absolute '
+          style={{
+            left: `${position.x}px`,
+            top: `${position.y}px`,
+            width: `${arrowSize}px`,
+            height: `${arrowSize}px`,
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <img
+            src="/arrowimage2.png"
+            className='block w-full h-full'
+          />
         </div>
+      </div>
     </div>
-    </>
   )
 }
 
