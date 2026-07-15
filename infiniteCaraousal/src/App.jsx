a1ccbee6af077edd8f4151d9f0c5ddb734df739b
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-15 21:40:03 +0530

    getting started with caraousal

--- infiniteCaraousal/src/App.jsx
+++ infiniteCaraousal/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import Caraousal from './components/Caraousal'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   
-
-     
+    <Caraousal/>
     </>
   )
 }
---
+++ infiniteCaraousal/src/components/Caraousal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Caraousal() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Caraousal
