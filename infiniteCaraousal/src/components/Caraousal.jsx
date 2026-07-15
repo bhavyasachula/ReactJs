844b1c8c735ec1d5aa71e2296cb2e9e87601bfc1
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-15 21:41:41 +0530

    Added 6 cards for caraousal

--- infiniteCaraousal/src/components/Caraousal.jsx
+++ infiniteCaraousal/src/components/Caraousal.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function Caraousal() {
   return (
     <>
-    
+    <div className='Caraousal'>
+        <div className='Group'>
+            <div className="card">1</div>
+            <div className="card">2</div>
+            <div className="card">3</div>
+            <div className="card">4</div>
+            <div className="card">5</div>
+            <div className="card">6</div>
+        </div>
+    </div>
     </>
   )
 }
