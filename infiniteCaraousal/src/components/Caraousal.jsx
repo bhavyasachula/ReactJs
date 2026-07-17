be786b04a0f30c42c455fc18740330a4da820f40
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-17 21:39:09 +0530

    Adding a new card

--- infiniteCaraousal/src/components/Caraousal.jsx
+++ infiniteCaraousal/src/components/Caraousal.jsx
@@ -19,7 +19,7 @@ function Caraousal() {
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732217.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/888/888835.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968382.png" alt="" /></div>
-        
+            <div className="card"></div>
         </div>
     </div>
     </>
