e5e1b243447a5f79a6e088a4d5164c089c959df8
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-17 21:44:43 +0530

    Adding apple logo

--- infiniteCaraousal/src/components/Caraousal.jsx
+++ infiniteCaraousal/src/components/Caraousal.jsx
@@ -12,7 +12,7 @@ function Caraousal() {
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/888/888835.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968382.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
-
+            <div className='card'><img src="" alt="" /></div>
         </div>
          <div className='Group'>
              <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732221.png" alt="" /></div>   
