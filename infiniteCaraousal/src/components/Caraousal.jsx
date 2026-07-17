443a18d647e9858bdf7330dc3c7268d9b6bf0f35
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-17 21:45:59 +0530

    Added new apple logo

--- infiniteCaraousal/src/components/Caraousal.jsx
+++ infiniteCaraousal/src/components/Caraousal.jsx
@@ -22,6 +22,8 @@ function Caraousal() {
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/888/888835.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968382.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
+            <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>
+
         </div>
     </div>
     </>
