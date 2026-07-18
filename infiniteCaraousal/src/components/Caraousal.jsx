7223a4d26b2ff3c70d48a9c782af1b37d6bfa9a8
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-18 23:27:31 +0530

    Getting started with updating

--- infiniteCaraousal/src/components/Caraousal.jsx
+++ infiniteCaraousal/src/components/Caraousal.jsx
@@ -15,7 +15,7 @@ function Caraousal() {
             <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>
         </div>
          <div className='Group'>
-             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732221.png" alt="" /></div>   
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732221.png" alt="" /></div>   
             <div className="card"><img src='https://cdn-icons-png.flaticon.com/512/5968/5968958.png' alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968776.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732217.png" alt="" /></div>
