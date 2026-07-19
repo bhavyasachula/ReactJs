7c6708636d9a78e2a42da27456506a3eec75d4a4
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-19 22:03:42 +0530

    Duplicated the logo

--- infiniteCaraousal/src/components/Caraousal.jsx
+++ infiniteCaraousal/src/components/Caraousal.jsx
@@ -20,6 +20,7 @@ function Caraousal() {
             <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/NVDA.webp" alt="" /></div>
             <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/SPCX.webp" alt="" /></div>
             <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/TSLA.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/TCS.NS.webp" alt="" /></div>
         </div>
          <div className='Group'>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732221.png" alt="" /></div>   
