0bccc79d5dd5f0a195dd0f48a7c6a9d872ce0889
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-03 23:48:01 +0530

    checking the websocketserver functionality

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -1,2 +1,8 @@
-import {WebSocketServer,WebSocket} from 'ws';
+const {WebSocketServer,WebSocket} = require('ws');
+const express = require('express');
+const app = express();
+const httpServer = app.listen(8080);
 
+const wss = new WebSocketServer({server:httpServer});
+
+console.log(wss);
