3a04bdeae4960a4c46921e8c5b3ae62682b4bfee
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-04 22:31:00 +0530

    making a simple button rn

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -6,7 +6,13 @@ const httpServer = app.listen(8080);
 const wss = new WebSocketServer({server:httpServer});
 
 app.get("/",(req,res)=>{
-    res.send("hello")
+    res.send(`
+    <html>
+      <body>
+        <input id="msg" placeholder="Type a message..." />
+        <button onclick="send()">Send</button>
+        <ul id="log"></ul>
+        <script>`)
 })
 wss.on("connection",(ws)=>{
     //whenever there is a connection control should reach here!
