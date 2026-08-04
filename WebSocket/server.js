5e5b38b3e5d83888b065ef49f166337c8289140c
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-04 22:29:55 +0530

    hello from the server

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -6,7 +6,7 @@ const httpServer = app.listen(8080);
 const wss = new WebSocketServer({server:httpServer});
 
 app.get("/",(req,res)=>{
-    
+    res.send("hello")
 })
 wss.on("connection",(ws)=>{
     //whenever there is a connection control should reach here!
